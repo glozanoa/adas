6ee06c578b50bc6afa9e1add4c1e4b48db319083
glozanoa <glozanoa@uni.pe>
2021-05-04 11:55:18 -0500

    update test of insertion algorithm

--- tests/sort/insertion.cpp
+++ tests/sort/insertion.cpp
@@ -1,65 +1,26 @@
 /*
  * Testing insertion sorting algorithm
  *
+ * Updated - date May 4 2021
+ *
  * Maintainer: glozanoa <glozanoa@uni.pe>
  */
 
 #include <iostream>
-#include <vector>
-#include <list>
 using namespace std;
 
 #include "../../include/sort.hpp"
+#include "../../include/io.hpp"
 #include "../../include/print.hpp"
-
-
-// COMPARISON FUNCTION WILL RETURN A BOOL : bool NAME_FUNCTION (ARGS_FUNCTION)
-bool lower_than(int x, int y)
-{
-  return x<y;
-}
 
 int main()
 {
+  vector<int> numbers = read::from_file("test/data/unsorted.txt")
+  bool verbose = true;
 
-  Timer time;
-  time.start();
+  vector<int> sorted = sort::insertion(numbers, verbose);
 
-  // READING NUMBERS FROM FILE ONLY FOR TESTING PURPOSES
-  ifstream fn("tests/data/random.txt");
-
-  vector<int> numbers;
-  int number;
-  string line;
-  while(getline(fn, line))
-    {
-      number = str2<int>(line);
-      numbers.push_back(number);
-    }
-
-  time.stop();
-  time.report("Elapsed time (read)");
-
-
-  // CREATE A COPY OF numbers TO TEST insertion WITH CUSTOM COMPARISON
-  vector<int> copy_numbers = vector<int>(numbers.size());
-  copy(numbers.begin(), numbers.end(), copy_numbers.begin());
-
-  cout << "insertion algorithm (default comparison)" << endl;
-  time.start();
-  // first booleam enable verbose, the second enable timer
-  sort::insertion(numbers.begin(), numbers.end(), true, true);
-  time.stop();
-  time.report("Elapsed time (sort - default comparison)");
-
-  cout << "insertion algorithm (custom comparison)" << endl;
-  time.start();
-  sort::insertion(copy_numbers.begin(), copy_numbers.end(), lower_than, true, true);
-  time.stop();
-  time.report("Elapsed time (sort - custom comparison)");
-
-
-  fn.close();
+  print::to_stdout("Sorted numbers:", sorted_numbers);
 
   return 0;
 }

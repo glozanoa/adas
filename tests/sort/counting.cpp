f9b0457c88fa67e98e125d8ac8fa2f17d1dbe792
glozanoa <glozanoa@uni.pe>
2021-05-04 12:06:56 -0500

    update test of counting algorithm

--- tests/sort/counting.cpp
+++ tests/sort/counting.cpp
@@ -1,6 +1,8 @@
 /*
  * Testing counting algorithm
  *
+ * Updated - date : May 4 2021
+ *
  * Maintainer: glozanoa <glozanoa@uni.pe>
  */
 
@@ -8,15 +10,16 @@
 using namespace std;
 
 #include "../../include/sort.hpp"
+#include "../../include/io.hpp"
 #include "../../include/print.hpp"
 
 
 int main()
 {
 
-  vector<int> numbers = {2, 5, 3, 0, 2, 3, 0, 3};
-
+  vector<int> numbers = read::from_file("tests/data/unsorted.txt")
   bool verbose = true;
+
   vector<int> sorted = sort::counting(numbers.begin(), numbers.end(), verbose);
 
   print::to_stdout("Sorted numbers:", sorted);

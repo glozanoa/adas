ec6c2963bb111866399e51fe8485c6ab26258d25
glozanoa <glozanoa@uni.pe>
2021-05-07 10:17:03 -0500

    upload tests of multithread sort algorithms

--- tests/parallel/mt_bibubble.cpp
+++ tests/parallel/mt_bibubble.cpp
@@ -1,8 +1,7 @@
 /*
- * Test of bubble parallel (multi thread) algorithm
+ * Test of bibubble multithread algorithm
  *
- * DEBUG EXECUTION WITH MORE THAN 2 THREADS (EXIT WITH ERROR) - SOLVED
- * Status - DEBUGGED (RUN SUCEFULLY WITH MORE THAN 2 THREADS) - date: May 7 2021
+ * Status: DEBUGGED - date : May 7 2021
  *
  * Maintainer: glozanoa <glozanoa@uni.pe>
  */
@@ -22,16 +21,15 @@ namespace ss = sort::serial;
 
 int main()
 {
-  // READING NUMBERS FROM FILE ONLY FOR TESTING PURPOSES
   vector<int> numbers = read::from_file<int>("tests/data/unsorted.txt");
   bool verbose = true;
   unsigned int nthreads = 3;
   Timer time;
 
   time.start();
-  sp::bubble(numbers.begin(), numbers.end(), nthreads, verbose);
+  sp::bibubble(numbers.begin(), numbers.end(), nthreads, verbose);
   time.stop();
-  time.report("Elapsed time (parallel bubble):");
+  time.report("Elapsed time (parallel selection):");
 
   print::to_stdout("Sorted vector:", numbers); // ONLY FOR DEBUGING PURPOSES
   //write::to_file(sorted.begin(), sorted.end(), "tests/data/sorted_bubble.txt");
--- tests/parallel/spm.cpp
+++ tests/parallel/spm.cpp
@@ -1,8 +1,5 @@
 /*
- * Test of bubble parallel (multi thread) algorithm
- *
- * DEBUG EXECUTION WITH MORE THAN 2 THREADS (EXIT WITH ERROR) - SOLVED
- * Status - DEBUGGED (RUN SUCEFULLY WITH MORE THAN 2 THREADS) - date: May 7 2021
+ * Test of spm general algorithm
  *
  * Maintainer: glozanoa <glozanoa@uni.pe>
  */
@@ -12,29 +9,23 @@
 using namespace std;
 
 #include "../../include/io.hpp"
-#include "../../include/timer.hpp"
-#include "../../include/print.hpp"
-
-
 #include "../../include/sort.hpp"
 namespace sp = sort::parallel;
 namespace ss = sort::serial;
 
+
 int main()
 {
-  // READING NUMBERS FROM FILE ONLY FOR TESTING PURPOSES
   vector<int> numbers = read::from_file<int>("tests/data/unsorted.txt");
-  bool verbose = true;
+  bool verbose = false;
   unsigned int nthreads = 3;
   Timer time;
 
   time.start();
-  sp::bubble(numbers.begin(), numbers.end(), nthreads, verbose);
+  sp::spm(numbers.begin(), numbers.end(), nthreads, ss::bubble<vector<int>::iterator>, verbose);
   time.stop();
   time.report("Elapsed time (parallel bubble):");
 
   print::to_stdout("Sorted vector:", numbers); // ONLY FOR DEBUGING PURPOSES
   //write::to_file(sorted.begin(), sorted.end(), "tests/data/sorted_bubble.txt");
-
-  return 0;
 }

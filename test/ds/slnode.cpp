2d8bb2095ae4772946e6bc3558a0bb07a2511961
glozanoa <glozanoa@uni.pe>
2021-05-15 08:42:40 -0500

    update CMakeLists file to compile sllist and slnode

--- test/ds/slnode.cpp
+++ test/ds/slnode.cpp
@@ -3,11 +3,15 @@
  * 
  */
 
-#include "adas/ds.hpp" //esto importa la clase SLNode(que esta en la carpeta include/adas/ds)
+#include <iostream>
+using namespace std;
+
+#include "adas/ds/single_linked_node.hpp" //esto importa la clase SLNode(que esta en la carpeta include/adas/ds)
 using namespace adas::ds;  // para que simplemente uses el nombre de la clase y ya no pongas en namespace
 
 
 int main()
 {
+	cout << "Probando si compila slnode.cpp file" << endl;
 	// COLOCA AQUI TU CODIGO QUE PRUEBE LOS DISTINTOS CONTRUCTORES Y METODOS DE LA CLASE SLNode
 }

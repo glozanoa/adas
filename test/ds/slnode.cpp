4edd7be66e5922aa1e3ca9fb3c8e829b3031154f
glozanoa <glozanoa@uni.pe>
2021-05-15 08:28:37 -0500

    adding templates to write tests

---
+++ test/ds/slnode.cpp
@@ -0,0 +1,13 @@
+/*
+ * PRIMERO ESCRIBE UNA PEQUEÑA DESCRIPCION DE LOS QUE CONTIENE EL ARCHIVO (BREVE DESCRIPCION, STATUS, MAINTAINER)
+ * 
+ */
+
+#include "adas/ds.hpp" //esto importa la clase SLNode(que esta en la carpeta include/adas/ds)
+using namespace adas::ds;  // para que simplemente uses el nombre de la clase y ya no pongas en namespace
+
+
+int main()
+{
+	// COLOCA AQUI TU CODIGO QUE PRUEBE LOS DISTINTOS CONTRUCTORES Y METODOS DE LA CLASE SLNode
+}

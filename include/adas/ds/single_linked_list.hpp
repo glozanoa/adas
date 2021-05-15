13f7299dfc8c434e46c5ab698b704b87fdcdca8a
glozanoa <glozanoa@uni.pe>
2021-05-15 08:17:24 -0500

    adding SLList and SLNode classes

--- include/adas/ds/single_linked_list.hpp
+++ include/adas/ds/single_linked_list.hpp
@@ -6,13 +6,74 @@
  * Maintiner: glozanoa <glozanoa@uni.pe>
  */
 
+#ifndef _SLLIST_H
+#define _SLLIST_H
+
+#include "single_linked_node.hpp"
+using namespace adas::ds;
+
 namespace adas::ds
 {
+  template<class T>
   class SLList
   /*
-   * SLLList : Single Linked List
+   * SLList : Single Linked List
    */
   {
+  protected:
+    SLNode<T>* head;
+    SLNode<T>* tail;
+    unsigned int size;
+  public:
+    SLList(): size{0}, head{nullptr}, tail{nullptr} {}
+    SLList(unsigned int list_size, T default_key)
+    /*
+     * Create a list of size list_size with default_key as node's key
+     */
+    {
+      head = new SLNode(default_key);
+      SLNode<T>* itr = head;
+      while(list_size > 0)
+        {
+          SLNode<T>* next_node = new SLNode(default_key);
+          itr.set_next(net_node);
+          itr = next_node;
+          list_size--;
+        }
+      tail = itr;
+    }
+
+    SLList(unsigned int list_size)
+    /*
+     * Create a list of size list_size with 0 as node's key
+     */
+    {
+      SLList(list_size, 0);
+    }
+
+    void push_back(SLNode<T>* node)
+    {
+      node->set_next(nullptr);
+      if(tail == nullptr && head = nullptr) // this is a empty list
+        {
+          head = node;
+          tail = node;
+        }
+      else
+        {
+          tail->set_next(node);
+          tail = node;
+        }
+    }
+
+    void push_front(SLNode<T>* node)
+    {
+      node->set_next(head);
+      head = node;
+    }
+
 
   };
 }
+
+#endif //_SLLIST_H
---
+++ include/adas/ds/single_linked_node.hpp
@@ -0,0 +1,52 @@
+/*
+ * Single Linked node template data structure (for implement SLList template class)
+ *
+ * Status:
+ *
+ * Maintiner: glozanoa <glozanoa@uni.pe>
+ */
+
+#ifndef _SLNODE_H
+#define _SLNODE_H
+
+namespace adas::ds
+{
+  template<class T>
+  class SLNode
+  /*
+   * SLNode : Single Linked Node
+   */
+  {
+  protected:
+    T key;
+    SLNode<T>* next;
+  public:
+    SLNode(T node_key): key{key_node}, next{nullptr} {}
+    void update_key(T node_key)
+    {
+      key = node_key;
+    }
+
+    void set_next(SLNode<T> next_node){next = next_node;}
+    void set_key(T node_key){key = node_key;}
+    SLNode<T>* get_next(){return next};
+    T get_key(){return key};
+
+    bool operation==(SLNode<T> node)
+      {
+        T node_key = node.get_key();
+        SLNode<T>* next_node = node.get_next();
+
+        if(key == node_key && next == next_node)
+          return true;
+        return false;
+      }
+    friend ostream& operation<<(ostream& out, SLNode<T> node)
+    {
+      out << "SLNode( key: " << node.get_key() << ", next: " << node.get_next() << ")" << endl;
+      return out;
+    }
+  };
+}
+
+#endif //_SLNODE_H

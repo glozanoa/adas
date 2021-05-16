9f906980517a3fd579fe160f678881f831d9990b
glozanoa <glozanoa@uni.pe>
2021-05-15 21:22:54 -0500

    update test of DLList<T> class

--- test/ds/dllist.cpp
+++ test/ds/dllist.cpp
@@ -9,6 +9,9 @@
 #include <iostream>
 using namespace std;
 
+#include "adas/utilities.hpp"
+namespace au = adas::utilities;
+
 #include "adas/ds/double_linked_list.hpp"
 using namespace adas::ds;
 
@@ -20,31 +23,15 @@ int main()
    nullptr <- a(key:1) <-> b (key:3) <-> c (key:0) -> nullptr
   */
 
+  cout << "Init empty list" << endl;
   DLList<int> list; //empty list
-  cout << "Empty list:" << endl;
-  cout << list << endl;
 
   list.push_back(3); // adding 1 element with key 3
 
-  cout << "After push back an element with key 3" << endl;
-  cout << list << endl;
+  au::print::to_stdout("After push back an element with key 3:\n", list, "\n");
 
   list.push_front(2);
-  cout << "After push front an element with key 2" << endl;
-  cout << list << endl;
-
-
-  DLList<int> other = DLList<int>(4, 1); //list of 4 nodes with key 1
-  cout << "Initialized list of 4 nodes with key 1" << endl;
-  cout << other << endl;
-
-  other.push_back(3); // adding 1 element with key 3
-  cout << "After push back an element with key 3" << endl;
-  cout << other << endl;
-
-  other.push_front(2);
-  cout << "After push back an element with key 2" << endl;
-  cout << other << endl;
+  au::print::to_stdout("After push front an element with key 2:\n", list, "\n");
 
   return 0;
 }

e0bc755c8f7a1444c354254953d88f4cfe4cb42c
glozanoa <glozanoa@uni.pe>
2021-05-21 17:41:02 -0500

    upload debugged test for SLNode<T> ds

--- test/ds/slnode.cpp
+++ test/ds/slnode.cpp
@@ -1,17 +1,40 @@
 /*
- * PRIMERO ESCRIBE UNA PEQUEÑA DESCRIPCION DE LOS QUE CONTIENE EL ARCHIVO (BREVE DESCRIPCION, STATUS, MAINTAINER)
- * 
+ * Testing SLNode template class
+ *
+ * Status: DEBUGED - date May 21 2021
+ *
+ * Maintainer: glozanoa <glozanoa@uni.pe>
  */
 
 #include <iostream>
 using namespace std;
 
-#include "adas/ds/single_linked_node.hpp" //esto importa la clase SLNode(que esta en la carpeta include/adas/ds)
-using namespace adas::ds;  // para que simplemente uses el nombre de la clase y ya no pongas en namespace
+#include "adas/ds/single_linked_node.hpp"
+using namespace adas::ds;
 
 
 int main()
 {
-	cout << "Probando si compila slnode.cpp file" << endl;
-	// COLOCA AQUI TU CODIGO QUE PRUEBE LOS DISTINTOS CONTRUCTORES Y METODOS DE LA CLASE SLNode
+  /*
+   * Node Structure
+   nullptr <- a(key:1) <-> b (key:3) <-> c (key:0) -> nullptr
+  */
+
+  SLNode<int>* a = new SLNode<int>(1);
+  SLNode<int>* b = new SLNode<int>(3);
+  SLNode<int>* c = new SLNode<int>(0);
+
+  cout << "a's key:" << a->get_key() << endl;
+  cout << "a DLNode (init):" << endl;
+  cout << a << endl;
+
+  a->set_next(b);
+  cout << "a DLNode (after linking with b):" << endl;
+  cout << a << endl;
+
+  b->set_next(c);
+  cout << "b DLNode (after linking with c):" << endl;
+  cout << b << endl;
+
+  return 0;
 }

9ce9d85ad2339f8c5d91e8fd38b1a642c0b31c24
glozanoa <glozanoa@uni.pe>
2021-05-21 17:39:50 -0500

    split definition and implementation of class SLNode<T>

--- include/adas/ds/single_linked_node.hpp
+++ include/adas/ds/single_linked_node.hpp
@@ -27,71 +27,96 @@ namespace adas::ds
     SLNode(T node_key, SLNode<T>* next_node)
       : key{node_key}, next{next_node} {}
 
-    void update_key(T node_key)
-    {
-      key = node_key;
-    }
 
+    // method to manipulate SLNode<T> objects
+    void update_key(T node_key){key = node_key;}
     void set_next(SLNode<T>* next_node){next = next_node;}
     void set_key(T node_key){key = node_key;}
     SLNode<T>* get_next(){return next;}
     SLNode<T>* get_node(){return this;}
     T get_key(){return key;}
+    static bool has_key(SLNode<T>* node, T node_key);
+    bool operator<(SLNode<T> node);
+    bool operator==(SLNode<T> node);
 
-    static bool has_key(SLNode<T>* node, T node_key)
-    /*
-     * Check if node has a key node_key
-     */
-    {
-      T key = node->get_key();
+    template<class L>
+    friend ostream& operator<<(ostream& out, SLNode<L> node);
 
-      return key == node_key;
-    }
-
-    static bool has_key(SLNode<T> node, T node_key)
-    /*
-     * Check if node has a key node_key
-     */
-    {
-      T key = node.get_key();
-
-      return key == node_key;
-    }
-
-    bool operator<(SLNode<T> node)
-    {
-      T node_key = node.get_key();
-      return key < node_key;
-    }
-
-    bool operator==(SLNode<T> node)
-      {
-        T node_key = node.get_key();
-        SLNode<T>* next_node = node.get_next();
-
-        if(key == node_key && next == next_node)
-          return true;
-        return false;
-      }
-
-    friend ostream& operator<<(ostream& out, SLNode<T> node)
-    {
-      T node_key = node.get_key();
-      SLNode<T>* next_node = node.get_next();
-
-      if(next_node != nullptr)
-        {
-          T next_node_key = next_node->get_key();
-
-          out << "SLNode(key: " << node_key
-              << ", next: " << next_node_key << ")";
-        }
-      else
-        out << "SLNode(key: " << node_key << ", next: NONE)";
-
-      return out;
-    }
+    template<class L>
+    friend ostream& operator<<(ostream& out, SLNode<L>* node);
   };
+
+  /*
+   * Class implementation
+   */
+
+  template<class T>
+  bool SLNode<T>::has_key(SLNode<T>* node, T node_key)
+  /*
+   * Check if node has a key node_key
+   */
+  {
+    T key = node->get_key();
+
+    return key == node_key;
+  }
+
+  template<class T>
+  bool SLNode<T>::operator<(SLNode<T> node)
+  {
+    T node_key = node.get_key();
+    return key < node_key;
+  }
+
+  template<class T>
+  bool SLNode<T>::operator==(SLNode<T> node)
+  {
+    T node_key = node.get_key();
+    SLNode<T>* next_node = node.get_next();
+
+    if(key == node_key && next == next_node)
+      return true;
+    return false;
+  }
+
+  template<class T>
+  ostream& operator<<(ostream& out, SLNode<T> node)
+  {
+    T node_key = node.get_key();
+    SLNode<T>* next_node = node.get_next();
+
+    if(next_node != nullptr)
+      {
+        T next_node_key = next_node->get_key();
+
+        out << "SLNode(key: " << node_key
+            << ", next: " << next_node_key << ")";
+      }
+    else
+      out << "SLNode(key: " << node_key << ", next: NONE)";
+
+    return out;
+  }
+
+  template<class T>
+  ostream& operator<<(ostream& out, SLNode<T>* node)
+  {
+    T node_key = node->get_key();
+    SLNode<T>* next_node = node->get_next();
+
+    if(next_node != nullptr)
+      {
+        T next_node_key = next_node->get_key();
+
+        out << "SLNode(key: " << node_key
+            << ", next: " << next_node_key << ")";
+      }
+    else
+      out << "SLNode(key: " << node_key << ", next: NONE)";
+
+    return out;
+  }
+
 }
 
 #endif //_SLNODE_H

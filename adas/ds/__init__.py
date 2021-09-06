0cc60a6cf24274c9aa57e2c0cc6eea630433474d
glozanoa <glozanoa@uni.pe>
2021-09-06 12:35:07 -0500

    enable DLNT importation from adas.ds module

--- adas/ds/__init__.py
+++ adas/ds/__init__.py
@@ -1,4 +1,4 @@
 from .tree_node import TNode
 from .single_linked_node import SLNode
 from .single_linked_list import SLList
-from .double_linked_node import DLNode
+from .double_linked_node import DLNode, DLNT
